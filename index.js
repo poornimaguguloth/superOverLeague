fcde1c5258d080048e789279c9fe3f288fb6df10
poornima <gugulothpoornima>
2023-04-15 16:41:21 +0000

    super-over-leagur

---
+++ index.js
@@ -0,0 +1,18 @@
+const element = (
+  //   Write your code here.
+  <div className="bg-container">
+    <h1 className="super-over-league-heading">Super Over Leagur</h1>
+    <div>
+      <img
+        className="logo"
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+      />
+      <img
+        className="logo"
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
